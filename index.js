9eccac795dc712135d5c1cb234f3a361b86f4cf2
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-12 14:59:00 -0700

    Implemented fetchMyIP function

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+// ISS Spotter
+// https://web.compass.lighthouselabs.ca/days/w02d4/activities/895
+
+const {fetchMyIP} = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+
+//   console.log(`IP is: `, ip);
+
+// });
