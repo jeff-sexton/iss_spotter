410394a8fc86157a6c4aa8a83b398c19208ea1a5
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-12 16:43:59 -0700

    Implemented coordination function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // ISS Spotter
 // https://web.compass.lighthouselabs.ca/days/w02d4/activities/895
 
-const {fetchMyIP, fetchCoordsByIP} = require('./iss');
+const {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation} = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -20,3 +20,23 @@ const {fetchMyIP, fetchCoordsByIP} = require('./iss');
 //   console.log(data);
 
 // });
+
+
+// fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, flyOvers) => {
+//   console.log(error);
+//   console.log(flyOvers);
+
+// });
+
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  passTimes.forEach(elem => {
+    const time = new Date(elem.risetime * 1000);
+    console.log(`Next pass at ${time.toLocaleString('en-US', {timeZone: 'UTC', timeZoneName: 'short'})} for ${elem.duration} seconds!`);
+    // How to print in PST?
+  });
+});
